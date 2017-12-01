38ff1059f5fb922a4e0e000e01044d838e891071
rileywaters <watersriley@gmail.com>
2017-11-30 22:16:36 -0800

    Project finalization

--- app.R
+++ app.R
@@ -1,9 +1,6 @@
 # Made by Riley Waters, Nov 2017 for STAT406 Environmentrics
 # Rocky Cities Environmetrics App
 
-# Yes, The whole thing is written in this one file.
-#Hit Run App in the upper right of this screen to run it.
-#You might need to open in browser and zoom out a bit to get the best experience
 
 
 # Automatically makes sure all required packages are installed and attached
@@ -71,7 +68,7 @@ ui <- dashboardPage(skin = "blue",
               fluidRow(
                 column(6, offset = 3,
                  p(style = "font-family: 'Source Sans Pro'; font-size: 18px",
-                   "This is an application for interacting with historical climate data from Vancouver BC, Kelowna BC, and Calgary AB. 
+                   "This is an application for interacting with historical temperature data from Vancouver BC, Kelowna BC, and Calgary AB. 
                    Despite being close in location, these cities experience unique climates due their positions relative to the Pacific Ocean, Okanagan Valley, Rocky Mountains, and Canadian Praries.
                    Data was collected by",
                    a("Climate Canada", href = "http://climate.weather.gc.ca/"),
@@ -82,14 +79,14 @@ ui <- dashboardPage(skin = "blue",
                  ),
                  tags$ul(style = "font-family: 'Source Sans Pro'; font-size: 16px",
                    tags$li("Sort and filter the data table"), 
-                   tags$li("Compare temperature and precipitation visualizations"), 
-                   tags$li("Fit models on the data and run randomization tests")
+                   tags$li("Compare temperature visualizations"), 
+                   tags$li("Run simulations and tests on the data")
                  ),
                  br(),
                  tags$i(style = "font-family: 'Source Sans Pro'; font-size: 16px",
                         "App created by Riley Waters for STAT406 Environmentrics.",
                         br(),
-                        "Data cleaned and compiled by Riley Waters. Last updated on November 23, 2017."
+                        "Data cleaned and compiled by Riley Waters. Last updated on November 29, 2017."
                  )
                 )
               ),
@@ -128,7 +125,7 @@ ui <- dashboardPage(skin = "blue",
       
       
       
-      # Temperatures tab content
+      # Monthly Content
       tabItem(tabName = "t1",
               fluidPage(
                 column(width = 6,
@@ -162,7 +159,7 @@ ui <- dashboardPage(skin = "blue",
               )
       ),
       
-      # Next Tab
+      # Monthly Advanced Content
       tabItem(tabName = "t2",
               fluidRow(
                 column(width = 6,
@@ -184,7 +181,7 @@ ui <- dashboardPage(skin = "blue",
               )
       ),
       
-      # Next Tab
+      # Density Content
       tabItem(tabName = "t3",
               fluidRow(
                 column(width = 6,
@@ -212,7 +209,7 @@ ui <- dashboardPage(skin = "blue",
               )
       ),
       
-      # T4 tab content
+      # Histogram Content
       tabItem(tabName = "t4",
               column(width = 6,
                 box(title = "Data Sample Selector", status = "warning", solidHeader = TRUE,collapsible = FALSE, collapsed = FALSE,width = 12,
@@ -232,6 +229,8 @@ ui <- dashboardPage(skin = "blue",
                 )
               )
       ),
+      
+      # Simple T-Tests Content
       tabItem(tabName = "m1",
               fluidRow(
                 column(width = 6,
@@ -296,7 +295,8 @@ ui <- dashboardPage(skin = "blue",
               )
               
       ),
-      # m2 tab content
+      
+      # Prediction Content
       tabItem(tabName = "m2",
               column(width = 6,
                      box(title = "Model Description", status = "warning", solidHeader = TRUE,collapsible = FALSE, collapsed = FALSE,width = 12,
@@ -325,7 +325,7 @@ ui <- dashboardPage(skin = "blue",
               )
       ),
       
-      # m3 tab content
+      # One Sample Simulation Content
       tabItem(tabName = "m3",
               fluidRow(
                 column(width = 6,
@@ -352,6 +352,7 @@ ui <- dashboardPage(skin = "blue",
               )
       ),
       
+      # Multiple Simulation Content
       tabItem(tabName = "m4",
               fluidRow(
                 column(width = 6,
@@ -363,13 +364,13 @@ ui <- dashboardPage(skin = "blue",
                        box(title = "Two Sample T-Test Results", status = "primary", solidHeader = TRUE,collapsible = FALSE, collapsed = FALSE,width = 12,
                            verbatimTextOutput("m5.Out")
                        ),
-                       box(title = "ANOVA results", status = "primary", solidHeader = TRUE,collapsible = FALSE, collapsed = FALSE,width = 12,
+                       box(title = "ANOVA Results", status = "primary", solidHeader = TRUE,collapsible = FALSE, collapsed = FALSE,width = 12,
                            verbatimTextOutput("m5.1.Out")
                        )
                 ),
                 
                 column(width = 6,
-                       box(title = "Simulated Data Plots", status = "primary", solidHeader = TRUE,collapsible = FALSE, collapsed = FALSE,width = 12,
+                       box(title = "Simulated Data Plot", status = "primary", solidHeader = TRUE,collapsible = FALSE, collapsed = FALSE,width = 12,
                            plotOutput("m5.2.Out")
                        ) 
                 )
@@ -399,6 +400,7 @@ server <- function(input, output, session) {
                         MeanDmin = round(mean(Min.Temp, na.rm = TRUE),1))
     return(monthly.df) 
   })
+  #Same, but for the next tab
   summarized2.df <- reactive({
     df <- subset(DataAll, Year >= input$range2[1] & Year <= input$range2[2])
     monthly.df <- ddply(df,.(City, Month), summarize,    
@@ -409,6 +411,7 @@ server <- function(input, output, session) {
                         MeanDmin = round(mean(Min.Temp, na.rm = TRUE),1))
     return(monthly.df) 
   })
+  #Used for tab 3
   summarized3.df <- reactive({
     df <- subset(DataAll, Year >= input$range3[1] & Year <= input$range3[2])
     if(input$opt.mmm2 == "meanT"){
@@ -425,6 +428,7 @@ server <- function(input, output, session) {
     }
     return(df) 
   })
+  #Used for tab4
   summarized4.df <- reactive({
     df <- subset(DataAll, Year >= input$range4[1] & Year <= input$range4[2])
     if(input$opt.mmm2 == "meanT"){
@@ -442,6 +446,7 @@ server <- function(input, output, session) {
     return(df) 
   })
   
+  # Constants for plots
   brk = c(seq(-30,30,10),50)
   label10s = c(as.character(seq(-30,30,10)))
   wx_range<-colorRampPalette(c(rgb(0,0.5,1), rgb(1,0.35,0) ))
@@ -472,7 +477,7 @@ server <- function(input, output, session) {
     }
   ))
   
-  # T1.1 Content
+  # Monthly Plot 1
   fun1.1 <- function(df, str.column.to.plot) {
     smalldf<-df
     if(str.column.to.plot == "meanT"){
@@ -500,7 +505,7 @@ server <- function(input, output, session) {
     print(mPlot)
   })
   
-  #T1.2 Content
+  # Monthly Plot 2
   fun1.2 <- function(df, str.column.to.plot) {
     if(str.column.to.plot == "meanT")
       str<-"Mean Temperature(C)"
@@ -524,7 +529,7 @@ server <- function(input, output, session) {
     print(mPlot)    
   })
   
-  # T1.21 Content
+  # Monthly Print 3
   output$t1.21.Out <- renderPrint({
     smalldf <- summarized.df()
     maxMax <- max(smalldf$maxT)
@@ -542,7 +547,7 @@ server <- function(input, output, session) {
     cat("The lowest low between", input$range[1], "and", input$range[2],"is", minMin, " Celsius, occuring on",paste(minDate),"in", paste(minName))
   })
   
-  # T1.3 Content
+  # Monthly Advanced Plot 1
   fun1.3 <- function(df) {
     p <- ggplot(df) 
     p <- p + geom_linerange(aes(x=City, 
@@ -569,7 +574,7 @@ server <- function(input, output, session) {
     print(MMbar)
   }, height=700)
   
-  # T1.4 Content
+  # Monthly Advanced Plot 2
   output$t1.4.Out <- renderPlot({    
     smalldf <- summarized2.df()
     p <- ggplot(data=smalldf, aes(x = factor(Month, levels=monthList),
@@ -584,7 +589,7 @@ server <- function(input, output, session) {
     print(p)
   }) 
   
-  # t1.5 Content
+  # Density Plot 1
   output$t1.5.Out <- renderPlot({
     df <- summarized3.df()
     if(input$opt.mmm2 == "meanT")
@@ -607,7 +612,7 @@ server <- function(input, output, session) {
     print(p)    
   })
   
-  #t1.6 content
+  # Density Plot 2
   output$t1.6.Out <- renderPlot({
     df <- summarized3.df()
     if(input$opt.mmm2 == "meanT")
@@ -634,7 +639,7 @@ server <- function(input, output, session) {
     print(p)  
   })
   
-  # t1.7 content
+  # Histogram Plot
   output$t1.7.Out <- renderPlot({
     df <- summarized4.df()
     if(input$opt.mmm2 == "meanT")
@@ -661,7 +666,7 @@ server <- function(input, output, session) {
     print(m)
   })
   
-  # m1 content
+  # Simple one-way T-test
   output$m1.Out <- renderPrint({
     if(input$m1opt == "Vancouver")
       df <- subset(DataAll, City == "Vancouver"& Year >=input$m1opt4[1] & Year <= input$m1opt4[2])
@@ -679,7 +684,7 @@ server <- function(input, output, session) {
     }
   })
   
-  # m2 Content
+  # Simple two-way T-test
   output$m2.Out <- renderPrint({
     if(length(input$m2opt) == 2){
       if(input$m2opt[1] == "Vancouver" & input$m2opt[2] =="Kelowna"){
@@ -702,22 +707,25 @@ server <- function(input, output, session) {
       print("Please select exactly two cities.")
   })
   
-  #m3 content
+  # Prediction model
   fitted <- reactive({
     df <- subset(DataAll, City == input$m3opt & Year >= 2000 & Year <= 2017)
     monthly.df <- ddply(df,.(Year, Month), summarize, meanT= mean(Mean.Temp, na.rm = TRUE))
     monthlyFit <- ts(monthly.df$meanT)
     fit <- auto.arima(monthlyFit)
   })
+  #Prediction summary
   output$m3.Out <- renderPrint({
     fit <- fitted()
     fit
   })
+  #Prediction Plot
   output$m3.1.Out <- renderPlot({
     fit <- fitted()
     fcast <- forecast(fit, h=18)
     plot(fcast, xlab = "YearMonth, starting with 0 = Jan 2000", ylab = "Temperature (C)", main = paste(input$m3opt, "Forecast"))
   })
+  #Prediction Results
   output$m3.2.Out <- renderPrint({
     fit <- fitted()
     p <- predict(fit, n.ahead = input$m3opt2)
@@ -725,7 +733,7 @@ server <- function(input, output, session) {
     
   })
   
-  #m4 content
+  # One sample simulation plot
   sim <- eventReactive(input$simulate,{
     df <- subset(DataAll, City == input$m4opt & Year >= 2000 & Year <= 2017)
     monthly.df <- ddply(df,.(Year, Month), summarize, meanT= mean(Mean.Temp, na.rm = TRUE))
@@ -737,6 +745,7 @@ server <- function(input, output, session) {
     sim <- sim()
     plot(sim,col='red', xlab = "YearMonth (Starting 0 = Jan 2000)", ylab = "Temperature (C)",main = paste(input$m4opt, "Simulation"))
   })
+  #One sample simulation results
   output$m4.1.Out <- renderPrint({
     y <- sim()
     if(is.na(input$m4opt3))
@@ -747,7 +756,7 @@ server <- function(input, output, session) {
     }
   })
   
-  #m5 content
+  #Multiple simulation
   simV <- eventReactive(input$simulate2,{
     df <- subset(DataAll, City == "Vancouver" & Year >= 2000 & Year <= 2017)
     monthly.df <- ddply(df,.(Year, Month), summarize, meanT= mean(Mean.Temp, na.rm = TRUE))
@@ -769,6 +778,7 @@ server <- function(input, output, session) {
     fit <- auto.arima(monthlyFit)
     sim <- simulate(fit,future=FALSE)
   })
+  #Two sample simulation results
   output$m5.Out <- renderPrint({
     if(length(input$m5opt) == 2){
       if(input$m5opt[1] == "Vancouver" & input$m5opt[2] =="Kelowna"){
@@ -791,6 +801,7 @@ server <- function(input, output, session) {
       print("Please select exactly two cities.")
     
   })
+  #Multiple simulation ANOVA results
   output$m5.1.Out <- renderPrint({
         sim <- simV()
         sim2 <- simK()
@@ -817,6 +828,8 @@ server <- function(input, output, session) {
         df <- rbind(df1, df2, df3)
         summary(aov(df$Mean~df$City))
   })
+  
+  #Multiple Simulation Plot
   output$m5.2.Out <- renderPlot({
     sim <- simV()
     sim2 <- simK()
@@ -825,9 +838,6 @@ server <- function(input, output, session) {
     lines(sim2,col='green')
     lines(sim3, col='blue')
     legend("topright", c("Vancouver", "Kelowna", "Calgary"),lty=c(1,1),lwd=c(2.5,2.5),col=c("red","blue","green"))
-    #lty(c(1,1))
-    #lwd=c(2.5,2.5)
-    #col=c("red","blue","green")
     })
  
   
